40bbf3bbfcef0a68bb159e56f75ce0249aafb754
zwebtheme <mdsuzonmia01@yahoo.com>
2017-04-22 01:59:40 +0600

    zwebtheme

---
+++ zgm.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @package    Article Custom Field
+ * @copyright  Copyright (c) 2016 web-tiki
+ * @license    GNU General Public License version 3, or later
+ */
+
+// Protect from unauthorized access
+defined ( '_JEXEC' ) or die ( 'Restricted access' );
+
+class plgContentZgm extends JPlugin {
+	protected $autoloadLanguage = true;
+
+	function onContentPrepareForm($form, $data)
+	{
+		$app = JFactory::getApplication();
+		$option = $app->input->get('option');
+		switch($option) {
+			case 'com_content':
+			if ($app->isAdmin())
+			{
+				JForm::addFormPath(__DIR__ . '/forms');
+				$form->loadFile('content', false);
+
+				$model = JModelLegacy::getInstance('Article', 'ContentModel');
+				$catid = $model->getItem($app->input->get('id'))->catid;
+
+				$pluginCatId = $this->params->get('category');
+
+				$document = JFactory::getDocument();
+				$document->addScriptDeclaration('
+				jQuery( document ).ready(function() {
+					var selectedCategory = jQuery("#jform_catid").val(),
+							catId = ' . $catid . ',
+							pluginCat = ' . $pluginCatId . ',
+							fields = jQuery("#attrib-zgm .controls"),
+							labels = jQuery("#attrib-zgm .control-label"),
+							warningMessage = jQuery("#attrib-zgm .control-group .zgm_warningMessage");
+
+
+					jQuery("#jform_catid").change(function() {
+						selectedCategory = jQuery("#jform_catid").val();
+						if(selectedCategory != pluginCat){
+							fields.hide();
+							labels.hide();
+							warningMessage.show();
+						} else {
+							fields.show();
+							labels.show();
+							warningMessage.hide();
+						}
+					});
+
+					if(selectedCategory != pluginCat){
+						fields.hide();
+						labels.hide();
+					} else {
+						warningMessage.hide();
+					}
+
+				});
+				');
+			}
+			return true;
+		}
+		return true;
+	}
+}
+?>
